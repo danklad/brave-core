b735a0d7b52125a079bfd30f0788b43b6f8a48fd
Josh Leonard <jleonard@brave.com>
2022-06-15 12:37:50 -0600

    fix: stop nav to new PW screen if wrong old PW

--- components/brave_wallet_ui/constants/types.ts
+++ components/brave_wallet_ui/constants/types.ts
@@ -277,6 +277,7 @@ export interface PageState {
   isCryptoWalletsInitialized: boolean
   isMetaMaskInitialized: boolean
   isImportWalletsCheckComplete: boolean
+  importWalletAttempts: number
 }
 
 export interface WalletPageState {
