eb2ccf3e03ab8fffbc5aee48cb3c04889f1244b0
Jay Harris <jay.harris@outlook.co.nz>
2022-06-28 13:09:19 +1200

    Expose a setting for configuring what Tab Hover Mode to use.
    Options are:
    1. Card (the default)
    2. Card with Preview
    3. Tooltip

---
+++ chromium_src/chrome/browser/ui/views/tabs/tab_strip.h
@@ -0,0 +1,21 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_STRIP_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_STRIP_H_
+
+#include "chrome/browser/ui/views/tabs/tab_slot_controller.h"
+
+#define UpdateHoverCard           \
+  UpdateHoverCard_Unused();       \
+  friend class BraveTabHoverTest; \
+  friend class BraveTabStrip;     \
+  void UpdateHoverCard
+
+#include "src/chrome/browser/ui/views/tabs/tab_strip.h"
+
+#undef UpdateHoverCard
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_STRIP_H_
