b373ae1ce0872f82941ea08567cb66d015b62a91
mkarolin <max@brave.com>
2022-07-13 20:40:06 -0400

    Puts client hints behind a feature flag.

--- browser/about_flags.cc
+++ browser/about_flags.cc
@@ -321,6 +321,12 @@ constexpr char kRestrictWebSocketsPoolName[] = "Restrict WebSockets pool";
 constexpr char kRestrictWebSocketsPoolDescription[] =
     "Limits simultaneous active WebSockets connections per eTLD+1";
 
+constexpr char kAllowCertainClientHintsName[] =
+    "Allow certain request client hints";
+constexpr char kAllowCertainClientHintsDescription[] =
+    "Allows setting certain request client hints (sec-ch-ua, sec-ch-ua-mobile, "
+    "sec-ch-ua-platform)";
+
 }  // namespace
 
 }  // namespace flag_descriptions
@@ -573,6 +579,10 @@ constexpr char kRestrictWebSocketsPoolDescription[] =
       flag_descriptions::kRestrictWebSocketsPoolName,                       \
       flag_descriptions::kRestrictWebSocketsPoolDescription, kOsAll,        \
       FEATURE_VALUE_TYPE(blink::features::kRestrictWebSocketsPool)},        \
+    {"allow-certain-client-hints",                                          \
+      flag_descriptions::kAllowCertainClientHintsName,                      \
+      flag_descriptions::kAllowCertainClientHintsDescription, kOsAll,       \
+      FEATURE_VALUE_TYPE(blink::features::kAllowCertainClientHints)},       \
     BRAVE_IPFS_FEATURE_ENTRIES                                              \
     BRAVE_NATIVE_WALLET_FEATURE_ENTRIES                                     \
     BRAVE_NEWS_FEATURE_ENTRIES                                              \
