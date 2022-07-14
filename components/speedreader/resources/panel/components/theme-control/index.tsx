45695e201e39ef7513155971efe4ed92b0589f67
Taher <taher.dasten@gmail.com>
2022-07-14 13:21:58 -0700

    added variant for theme options

--- components/speedreader/resources/panel/components/theme-control/index.tsx
+++ components/speedreader/resources/panel/components/theme-control/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 import * as S from './style'
+import classnames from '$web-common/classnames'
 
 const themeOptions = [
   {
@@ -18,16 +19,36 @@ const themeOptions = [
 ]
 
 function ThemeControl () {
+  const [activeTheme] = React.useState('dark')
+
   return (
-    <S.Box>
+    <S.Box role="listbox">
       {themeOptions.map(entry => {
+        const chipClass = classnames({
+          'chip': true,
+          'is-active': activeTheme === entry.name
+        })
+
+        const iconClass = classnames({
+          'icon-box': true,
+          [`is-${entry.name}`]: true
+        })
+
         return (
-          <S.Chip>
+          <button
+            role="option"
+            className={chipClass}
+            aria-selected={activeTheme === entry.name}
+            aria-label={entry.name}
+          >
             <div
-              className={`is-${entry.name}`}
+              className={iconClass}
               dangerouslySetInnerHTML={{ __html: entry.svgIcon }}
             />
-          </S.Chip>
+            {activeTheme === entry.name && (
+              <i><svg width="18" height="18" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 17.333C4.405 17.333.667 13.595.667 9 .667 4.405 4.405.667 9 .667c4.595 0 8.333 3.738 8.333 8.333 0 4.595-3.738 8.333-8.333 8.333Zm-.172-5.098a.691.691 0 0 1-.956.085L4.4 9.542a.695.695 0 0 1 .867-1.084L8.22 10.82l4.424-5.055a.695.695 0 1 1 1.045.914l-4.861 5.556Z" fill="#737ADE"/></svg></i>
+            )}
+          </button>
         )
       })}
     </S.Box>
--- components/speedreader/resources/panel/components/theme-control/style.ts
+++ components/speedreader/resources/panel/components/theme-control/style.ts
@@ -4,16 +4,32 @@ export const Box = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-gap: 6px;
-`
 
-export const Chip = styled.div`
-  margin: 0 auto;
-  width: auto;
-  height: 48px;
-  border-radius: 8px;
-  border: 1px solid transparent;
+  .chip {
+    --border-color: transparent;
 
-  div {
+    margin: 0 auto;
+    width: auto;
+    height: 48px;
+    background: transparent;
+    border-radius: 8px;
+    border: 1px solid var(--border-color);
+    position: relative;
+    padding: 0;
+    cursor: pointer;
+  }
+
+  i {
+    position: absolute;
+    right: 6px;
+    bottom: 4px;
+  }
+
+  .chip.is-active {
+    --border-color: rgba(255, 255, 255, 0.1);
+  }
+
+  .icon-box {
     width: 100%;
     height: 100%;
     background: transparent;
@@ -21,15 +37,7 @@ export const Chip = styled.div`
     overflow: hidden;
   }
 
-  .is-light {
-    background: #F6F6F9;
-  }
-
-  .is-dark {
-    background: #17171F;
-  }
-
-  .is-sepia {
-    background: #E9E0CA;
-  }
+  .is-light { background: #F6F6F9; }
+  .is-dark { background: #17171F; }
+  .is-sepia { background: #E9E0CA; }
 `
