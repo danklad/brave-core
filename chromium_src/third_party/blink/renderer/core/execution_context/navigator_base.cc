eedc83c44654f38ebfaf1e60ac1d018e5b8e2ca3
mkarolin <max@brave.com>
2022-07-18 12:35:15 -0400

    fixup! Navigator.hardwareConcurrency is now an override.

--- chromium_src/third_party/blink/renderer/core/execution_context/navigator_base.cc
+++ chromium_src/third_party/blink/renderer/core/execution_context/navigator_base.cc
@@ -18,11 +18,10 @@
 namespace blink {
 namespace probe {
 
-const unsigned kFakeMinProcessors = 2;
-const unsigned kFakeMaxProcessors = 8;
-
 void ApplyBraveHardwareConcurrencyOverride(blink::ExecutionContext* context,
                                            unsigned int& hardware_concurrency) {
+  const unsigned kFakeMinProcessors = 2;
+  const unsigned kFakeMaxProcessors = 8;
   unsigned true_value =
       static_cast<unsigned>(base::SysInfo::NumberOfProcessors());
   if (true_value <= 2) {
